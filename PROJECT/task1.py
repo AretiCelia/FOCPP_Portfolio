437a1da93c8689e3785566f576bfb15e9594547a
Priska Dangol <dpriska24@tbc.edu.np>
2026-01-16 18:21:31 +0545

    Final Submission

---
+++ PROJECT/task1.py
@@ -0,0 +1,29 @@
+'''Beckett Pizza Plaza 4-for-3 Offer. This program will calculate the 
+total price for four pizzas where the cheapest pizza is free (4-for-3 offer)'''
+
+print("Beckett Pizza Plaza 4-for-3 Offer")
+print("=" * 33)
+
+prices = []
+pizza_num = 1
+
+while len(prices) < 4:
+    try:
+        price = float(input(f"Enter Price of Pizza #{pizza_num}: "))
+
+        if price <= 0:
+            print("Please enter a valid price!")
+            continue
+
+        prices.append(price)
+        pizza_num += 1
+
+    except ValueError:
+        print("Please enter a valid price!")
+
+original_total = sum(prices)
+cheapest = min(prices)
+total = original_total - cheapest
+discount = (cheapest / original_total) * 100
+
+print(f"\nOrder Total is £{total:.2f}, a fabulous discount of {discount:.0f}%!")
---
+++ PROJECT/task2.py
@@ -0,0 +1,24 @@
+''' a program that asks a user to enter their password, checks its length, 
+and then verifies it further by asking for various characters from it.'''
+
+import random
+
+password = input("Enter your password: ")
+
+if len(password) < 9:
+    print("Password too short.")
+    exit()
+
+length = len(password)
+
+for _ in range(3):
+    check_position = random.randint(1, length)
+    guess = input(f"\nEnter letter at position {check_position}: ")
+
+    if guess == password[check_position - 1]:
+        print("Correct")
+    else:
+        print("Security check failed.")
+        exit()
+
+print("Security check passed.")
